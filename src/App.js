7dd5fcc60b0bd730a8e3da184d5317ce8995abe4
Trevor Mearns <spiral272@gmail.com>
2021-06-10 17:10:07 +0900

    first commit

---
+++ src/App.js
@@ -0,0 +1,16 @@
+import React from 'react';
+// import ReactDOM from 'react-dom';
+// import ReactWizard from 'react-bootstrap-wizard';
+// import { Container, Row, Col } from 'reactstrap';
+import Wizard from './Wizard';
+import './App.css';
+
+function App() {
+  return (
+    <div className='App'>
+      <Wizard />
+    </div>
+  );
+}
+
+export default App;
